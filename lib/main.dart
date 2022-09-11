1d7edc3222d3ddcc46f423e85f35e218fc161844
istaqom <istaqom@gmail.com>
2022-09-11 10:15:33 +0800

    add icon and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -37,17 +37,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'BlankPassword',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: buildMaterialColor(const Color(0xffd1512d)),
         scaffoldBackgroundColor: buildMaterialColor(const Color(0xffd1512d)),
+        fontFamily: 'Monsterrat',
       ),
       home: const MyHomePage(title: 'BlankPassword'),
     );
@@ -75,18 +67,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
       body: Column(
         children: <Widget>[
           const Text(
