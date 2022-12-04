6b18f55ed7494ba92b01930f203b0eeb2de7fac6
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2022-12-04 22:40:43 +0800

    fix(app): make text ellipsis on overflow

--- app/lib/credential/view/credential_list.dart
+++ app/lib/credential/view/credential_list.dart
@@ -1,15 +1,18 @@
 import 'package:blankpassword/credential/blocs/credential_bloc.dart';
 import 'package:blankpassword/credential/blocs/credentials_bloc.dart';
 import 'package:blankpassword/credential/view/credential_widget.dart';
+import 'package:credential_repository/credential_repository.dart';
 import 'package:flutter/material.dart';
 
 class CredentialListWidget extends StatelessWidget {
   const CredentialListWidget({
     super.key,
     required this.bloc,
+    required this.onCredentialPressed,
   });
 
   final CredentialsBloc bloc;
+  final void Function(Credential) onCredentialPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -25,19 +28,7 @@ class CredentialListWidget extends StatelessWidget {
                 ),
               ),
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return CredentialWidget(
-                        bloc: CredentialBloc(
-                          credentialBloc: bloc,
-                          credential: item,
-                        ),
-                      );
-                    },
-                  ),
-                );
+                onCredentialPressed(item);
               },
               child: Row(
                 children: [
@@ -46,24 +37,26 @@ class CredentialListWidget extends StatelessWidget {
                     color: Color(0xff472D2D),
                     size: 50,
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        item.name,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          color: Color(0xff472D2D),
-                          fontSize: 20,
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          item.name,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            color: Color(0xff472D2D),
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                      Text(
-                        item.username,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    ],
+                        Text(
+                          item.username,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
